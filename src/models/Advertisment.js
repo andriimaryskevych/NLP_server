fb8c9a8106f00f9be6dca873f4cfb4a5a62bad1a
Andrii Maryskevych <andrii.maryskevych@gmail.com>
2018-12-11 20:22:51 +0200

    Added Models for ads

---
+++ src/models/Advertisment.js
@@ -0,0 +1,21 @@
+'use strict';
+
+class Advertisment {
+    constructor (
+        name,
+        image,
+        price,
+        adId,
+        carStore
+    ) {
+        Object.assign(this, {
+            name,
+            image,
+            price,
+            adId,
+            carStore
+        });
+    }
+};
+
+module.exports = Advertisment;
---
+++ src/models/RichAdvertisment.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Advertisment = require('./Advertisment');
+
+class RichAdvertisment extends Advertisment {
+    constructor (
+        name,
+        image,
+        price,
+        adId,
+        carStore,
+        description
+    ) {
+        super(
+            name,
+            image,
+            price,
+            adId,
+            carStore
+        );
+
+        Object.assign(this, {
+            description
+        });
+    }
+};
+
+module.exports = RichAdvertisment;
