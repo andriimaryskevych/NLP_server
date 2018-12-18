619423c5ca213bf4096c52ef1ad989fb4de2610b
Andrii Maryskevych <andrii.maryskevych@gmail.com>
2018-12-18 09:54:58 +0200

    Adding dialg flow fullfilment

--- src/api/dialogflow/dialogflow.controller.js
+++ src/api/dialogflow/dialogflow.controller.js
@@ -1,6 +1,36 @@
 'use strict';
 
+const { WebhookClient } = require('dialogflow-fulfillment');
+
+const DIALOGFLOW_ENTITIES = {
+    MODEL: 'car_model',
+    MARK: 'car_mark',
+    PRICE_TO: 'number',
+    PRICE_FROM: 'number-integer'
+}
+
+const close = agent => {
+    const params = agent.contexts[0].parameters;
+
+    const res = {};
+
+    Object.entries(DIALOGFLOW_ENTITIES).forEach(([key, value]) => {
+        res[key] = params[value];
+    });
+
+    console.log(res);
+
+    agent.add(`Welcome to Express.JS webhook!`);
+};
+
 exports.message = (req, res) => {
     console.log('Incomming Dialogflow request');
-    res.send('OK');
+    console.log(req.body);
+
+    const agent = new WebhookClient({request: req, response: res});
+
+    let intentMap = new Map();
+    intentMap.set('apply_filters', close);
+
+    agent.handleRequest(intentMap);
 };
